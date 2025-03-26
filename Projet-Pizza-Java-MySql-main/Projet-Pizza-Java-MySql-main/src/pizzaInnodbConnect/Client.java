89ea418779c551490179af8c0df3a5702a601830
MAKAN NJOBEN Jordan <jmakannjoben@gmail.com>
2025-03-26 12:32:02 +0100

    Les codes des différentes classes de ce projet

---
+++ Projet-Pizza-Java-MySql-main/Projet-Pizza-Java-MySql-main/src/pizzaInnodbConnect/Client.java
@@ -0,0 +1,13 @@
+package pizzaInnodbConnect;
+public class Client {
+	
+	int numeroClient; //Dans la base de c'est NROCLIE
+	String nameClient; //Dans la base c'est NOMCLIE
+	
+	public Client (int numeroClient, String nameClient) {
+		super ();
+		this.numeroClient = numeroClient ;
+		this.nameClient = nameClient ;
+	}
+
+}
